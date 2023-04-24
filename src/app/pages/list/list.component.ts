8b64e960cea67392ce950e32d4b101316938bd0c
Cinthiapiera <cinthia.maldonado@upch.pe>
2023-04-23 21:18:20 -0500

    list cards

---
+++ src/app/pages/list/list.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit} from '@angular/core';
+import { Card } from 'src/app/interfaces/card.interface';
+import { CardService } from 'src/app/services/card.service';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent implements OnInit{
+
+  cards: Card[] = [];
+
+  constructor(private cardService: CardService){}
+
+  ngOnInit(): void {
+    this.cardService.getCards().subscribe(res=>{
+      console.log(res);
+      this.cards = res;
+
+    })
+  }
+
+}
